05c6a80875bdbc2cfa59ed49ff15b527d704ef65
Hemanth Marella <hemanthmarella2108@gmail.com>
2026-07-05 21:45:13 +0530

    separate middleware and running file

--- AiAssist/api/middlewares.py
+++ AiAssist/api/middlewares.py
@@ -1,20 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ..Routers.pdf_router import router as pdf_router
 
+def configure_middleware(app: FastAPI):
+    origins = [
+        "https://aurix-engine-study-helper-ai.netlify.app"
+    ]
 
-app = FastAPI()
-
-origins = [
-    'https://aurix-engine-study-helper-ai.netlify.app'
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-app.include_router(pdf_router, prefix="/Aurix/api/v1")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .AiAssist.Routers.pdf_router import router as pdf_router
+from .AiAssist.api.middlewares import configure_middleware
+
+app = FastAPI()
+
+# Register middleware
+configure_middleware(app)
+
+# Register routers
+app.include_router(pdf_router, prefix="/Aurix/api/v1")
